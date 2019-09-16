313ea9fb29c5b6fb8df63f287150b09d1bb41950
Kundan Joshi <kundan.joshi1@gmail.com>
2019-09-17 00:27:37 +0530

    Updates

---
+++ precache-manifest.8b4c3c2ada370edbb7a559c9fdf91a48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa58e9cf528651c884e24e115e44543",
+    "url": "/simply-crossword/index.html"
+  },
+  {
+    "revision": "8fc2013e9469ea0dbae9",
+    "url": "/simply-crossword/static/css/main.2aa341e6.chunk.css"
+  },
+  {
+    "revision": "09f30aea80905c118a9e",
+    "url": "/simply-crossword/static/js/2.9bdd56a0.chunk.js"
+  },
+  {
+    "revision": "8fc2013e9469ea0dbae9",
+    "url": "/simply-crossword/static/js/main.9b8af037.chunk.js"
+  },
+  {
+    "revision": "92f5e5469523db518b0b",
+    "url": "/simply-crossword/static/js/runtime~main.e5dc1c36.js"
+  }
+]);
